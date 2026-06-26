1a805bb5c851116586496e91ed9a7bee615ae1b3
buenoIago <iagocedup@gmail.com>
2026-06-26 17:55:54 -0300

    feat: adiciona injeção de dependência do ModuloContato

--- E-Agenda.WebApp/Compartilhado/Aplicacao/InjecaoDependencia.cs
+++ E-Agenda.WebApp/Compartilhado/Aplicacao/InjecaoDependencia.cs
@@ -1,9 +1,6 @@
 using E_Agenda.WebApp.Modulos.ModuloCategoria.Aplicacao;
 using E_Agenda.WebApp.Modulos.ModuloDespesas.Aplicacao;
-using E_Agenda.WebApp.Compartilhado.Dominio;
 using E_Agenda.WebApp.Compartilhado.Infra.Arquivos;
-using E_Agenda.WebApp.Modulos.ModuloContatos.Dominio;
-using E_Agenda.WebApp.Modulos.ModuloContatos.Infra;
 using E_Agenda.WebApp.Modulos.ModuloTarefa.Aplicacao;
 
 
@@ -16,7 +13,6 @@ public static class InjecaoDependencia
         services.AddScoped<ServicoTarefa>();
 
         services.AddSingleton<ContextoJson>();
-        services.AddScoped<IRepositorio<Contatos>, RepositorioContatosEmArquivo>();
         services.AddScoped<ServicoContatos>();
     }
 }
--- E-Agenda.WebApp/Compartilhado/Infra/InjecaoDependencia.cs
+++ E-Agenda.WebApp/Compartilhado/Infra/InjecaoDependencia.cs
@@ -1,6 +1,9 @@
+using E_Agenda.WebApp.Compartilhado.Dominio;
 using E_Agenda.WebApp.Compartilhado.Infra.Arquivos;
 using E_Agenda.WebApp.Modulos.ModuloCategoria.Dominio;
 using E_Agenda.WebApp.Modulos.ModuloCategoria.Infra;
+using E_Agenda.WebApp.Modulos.ModuloContatos.Dominio;
+using E_Agenda.WebApp.Modulos.ModuloContatos.Infra;
 using E_Agenda.WebApp.Modulos.ModuloDespesas.Dominio;
 using E_Agenda.WebApp.Modulos.ModuloDespesas.Infra;
 using E_Agenda.WebApp.Modulos.ModuloTarefa.Dominio;
@@ -12,11 +15,10 @@ public static class InjecaoDependencia
 {
     public static void AddInfraRepositories(this IServiceCollection services)
     {
-   
-
         services.AddScoped<IRepositorioCategoria, RepositorioCategoriaEmArquivo>();
         services.AddScoped<IRepositorioDepesa, RepositorioDespesaEmArquivo>();
         services.AddScoped<IRepositorioTarefa, RepositorioTarefaEmArquivo>();
 
+        services.AddScoped<IRepositorio<Contatos>, RepositorioContatosEmArquivo>();
     }
 }
