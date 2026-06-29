bb2fc99eec2e863d687edd3d9d6e56cef8fb25bf
buenoIago <iagocedup@gmail.com>
2026-06-28 22:29:22 -0300

    fix: renomeia Colunas do banco

--- .sql/db_schema.sql
+++ .sql/db_schema.sql
@@ -17,30 +17,30 @@ END;
 
 IF OBJECT_ID('dbo.TBDespesa', 'U') IS NULL
 BEGIN
-CREATE TABLE [dbo].[TBDespesa] (
-[Id] uniqueidentifier NOT NULL,
-[Descrição] nvarchar(100) NOT NULL,
-[Data de ocorrência] datetime NOT NULL,
-[Valor] decimal(18,0) NOT NULL,
-[Forma de Pagamento] nvarchar(7) NOT NULL,
-[CategoriaId] uniqueidentifier NOT NULL,
-PRIMARY KEY ([Id])
-);
+    CREATE TABLE [dbo].[TBDespesa] (
+        [Id] uniqueidentifier NOT NULL,
+        [Descricao] nvarchar(100) NOT NULL,
+        [DataOcorrencia] datetime2(0) NOT NULL,
+        [Valor] decimal(18,0) NOT NULL,
+        [FormaPagamento] nvarchar(7) NOT NULL,
+        [CategoriaId] uniqueidentifier NOT NULL,
+        PRIMARY KEY ([Id])
+    );
 END;
 
 IF OBJECT_ID('dbo.TBTarefa', 'U') IS NULL
 BEGIN
-CREATE TABLE [dbo].[TBTarefa] (
-[Id] uniqueidentifier NOT NULL,
-[Título] nvarchar(100) NOT NULL,
-[Prioridade da Tarefa] nvarchar(6) NOT NULL,
-[Data de Criação] datetime NOT NULL,
-[Data de Conclusão] datetime,
-[Status de Conclusão] nvarchar(50) NOT NULL,
-[Percentual Concluído] int NOT NULL,
-[Item de Tarefa] uniqueidentifier NOT NULL,
-PRIMARY KEY ([Id])
-);
+    CREATE TABLE [dbo].[TBTarefa] (
+        [Id] uniqueidentifier NOT NULL,
+        [Titulo] nvarchar(100) NOT NULL,
+        [PrioridadeTarefa] nvarchar(6) NOT NULL,
+        [DataCriacao] datetime2(0) NOT NULL,
+        [DataConclusao] datetime2(0),
+        [StatusConclusao] nvarchar(50) NOT NULL,
+        [PercentualConcluido] int NOT NULL,
+        [ItemTarefa] uniqueidentifier NOT NULL,
+        PRIMARY KEY ([Id])
+    );
 END;
 
 IF NOT EXISTS (
