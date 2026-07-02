cfa15390cec72f898f745f0b3691e4b561ea2250
Dayuã-Santana <daniponsantana@gmail.com>
2026-07-02 00:34:00 -0300

    fix:Corrigindo bugs

--- E-Agenda.WebApp/Modulos/ModuloContatos/Dominio/ContatoProfile.cs
+++ E-Agenda.WebApp/Modulos/ModuloContatos/Dominio/ContatoProfile.cs
@@ -8,19 +8,23 @@ public class ContatosProfile : Profile
 {
     public ContatosProfile()
     {
-        // Se você não tiver uma ViewModel de Listagem específica, 
-        // use a FuncionarioViewModel para tudo
-        CreateMap<Contatos, CadastrarContatosViewModel>().ReverseMap();
-        CreateMap<CadastrarContatosViewModel, Contatos>();
-        CreateMap<Contatos, ExcluirContatosViewModel>();
-        CreateMap<Contatos, EditarContatosViewModel>().ReverseMap();
-        CreateMap<Contatos, ListarContatosViewModel>();
-        CreateMap<Contatos, VisualizarContatoViewModel>();
+        // Mapeamentos para Cadastrar e Editar
+        // Usamos .ForMember(..., opt => opt.Ignore()) para o AutoMapper não tentar
+        // converter a lista de Checkboxes para a lista de Entidades de Compromisso.
+        
+        CreateMap<CadastrarContatosViewModel, Contatos>()
+            .ForMember(dest => dest.Compromissos, opt => opt.Ignore());
+            
+        CreateMap<Contatos, CadastrarContatosViewModel>();
 
         CreateMap<EditarContatosViewModel, Contatos>()
             .ForMember(dest => dest.Compromissos, opt => opt.Ignore());
             
         CreateMap<Contatos, EditarContatosViewModel>();
 
+        // Demais mapeamentos
+        CreateMap<Contatos, ExcluirContatosViewModel>();
+        CreateMap<Contatos, ListarContatosViewModel>();
+        CreateMap<Contatos, VisualizarContatoViewModel>();
     }
 }
